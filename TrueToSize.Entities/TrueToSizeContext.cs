8d74896a76f58720f18b3183fd53e8239da2dd23
Rob Gafken <rtd91@live.com>
2019-09-17 19:29:26 -0400

    Create EFCore DBContext

---
+++ TrueToSize.Entities/TrueToSizeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace TrueToSize.Entities
+{
+    public class TrueToSizeContext : DbContext
+    {
+        //Tie Postgres to EFCore :)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql("");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Register Models Here
+        }
+    }
+}
