a20fe816c2820d1127e58c1a33473e4babc17334
Rob Gafken <rtd91@live.com>
2019-09-17 21:19:57 -0400

    Start wiring up Docker for Postgres connection support - Functional testing still needed

--- TrueToSize/Startup.cs
+++ TrueToSize/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using TrueToSize.Entities;
 
 namespace TrueToSize
 {
@@ -25,7 +27,11 @@ namespace TrueToSize
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var configurationSetting = new ConfigurationBuilder().Build();
+            services.AddSingleton(configurationSetting);
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContextPool<TrueToSizeContext>(options => options.UseSqlServer(configurationSetting.GetConnectionString("DataAccessPostgreSqlProvider")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
